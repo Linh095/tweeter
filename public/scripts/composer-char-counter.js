8724e3cd096fa130b70dc06dc7a9f199e85eceda
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-03-25 17:05:37 -0400

    added some jquery for counting character

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,26 @@
+$(document).ready( ()=> {
+  console.log("DOM is ready!");
+  alert("DOM is ready!");
+});
+
+const MAXCHARACTERS = 140;
+// Function that updates character counter when user inputs text into new tweet textArea
+const characterCounter = () => {
+  $("#tweet-text").on("input", function() {
+    // get length text in textarea
+    const textLength = $(this).val().length;
+    // select counter output element
+    const counter = $(this).siblings(".counter");
+    // calculate remaining characters
+    const count = MAXCHARACTERS - textLength;
+    // Add class when maximum character count has been reached, remove if above again
+    count < 0 ? counter.addClass("over") : counter.removeClass("over");
+    // update counter value
+    counter.val(count);
+    console.log(count);
+  });
+};
+
+export default characterCounter;
+
+
