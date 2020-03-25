e55cba961b38671d3b7afb9e0991957a841c9b40
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-03-25 12:33:09 -0400

    edited styles in nave header and new-tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+composer-char-counter.js
+const MAXCHARACTERS = 140;
+// Function that updates character counter when user inputs text into new tweet textArea
+const characterCounter = () => {
+  $("#tweet-text").on("input", function() {
+    // get length text in textarea
+    const textLength = $(this).val().length;
+    // select counter output element
+    const counter = $(this).siblings(".counter");
+    // calculate remaining characters
+    const count = MAXCHARACTERS - textLength;
+    // Add class when maximum character count has been reached, remove if above again
+    count < 0 ? counter.addClass("over") : counter.removeClass("over");
+    // update counter value
+    counter.val(count);
+  });
+};
+export default characterCounter;
+
+client.js
+import characterCounter from "./composer-char-counter.js";
+// Function to run when page is ready
+const onReady = () => {
+  characterCounter();
+};
+// Run when page is ready
+$(document).ready(onReady);
+
+modules.exports ={}
+
+export { funcOne, varOne, FuncTwo ... }
+export default funcOne;
