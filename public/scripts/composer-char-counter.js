17b1b4acde1854d16980ea7a3e313defd08b60ad
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-03-25 19:18:24 -0400

    removed debugging lines

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -9,15 +9,15 @@ const characterCounter = () => {
   $("#tweet-text").on("input", function() {
     // get length text in textarea
     const textLength = $(this).val().length;
-    // select counter output element
-    const counter = $(this).siblings(".counter");
+    
     // calculate remaining characters
     const count = MAXCHARACTERS - textLength;
-    // Add class when maximum character count has been reached, remove if above again
+    
+    // Add class to output when maximum character count has been reached, remove if above again
+    const counter = $(".counter");
     count < 0 ? counter.addClass("over") : counter.removeClass("over");
     // update counter value
     counter.val(count);
-    console.log(count);
   });
 };
 
