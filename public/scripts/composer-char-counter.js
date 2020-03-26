c46a76ccc8354795d716a0e0fd84575d5130ae18
Linh Vu Hai Nguyen <linh.nguyen.nvhl@gmail.com>
2020-03-26 00:15:36 -0400

    made styles for displaying tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,3 @@
-$(document).ready( ()=> {
-  console.log("DOM is ready!");
-  alert("DOM is ready!");
-});
-
 const MAXCHARACTERS = 140;
 // Function that updates character counter when user inputs text into new tweet textArea
 const characterCounter = () => {
@@ -12,7 +7,7 @@ const characterCounter = () => {
     
     // calculate remaining characters
     const count = MAXCHARACTERS - textLength;
-    
+
     // Add class to output when maximum character count has been reached, remove if above again
     const counter = $(".counter");
     count < 0 ? counter.addClass("over") : counter.removeClass("over");
